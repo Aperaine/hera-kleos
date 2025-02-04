b923517bf54874e10bae9e11246ae7ee7fd48bdb
Umarik <umarbektokyo@icloud.com>
2025-02-04 16:52:20 +0900

    Created basic templates for Heracle and Castle.
    - Created basic caste scene with the floor and parallax effect.
    - Added the camera and made it follow Heracle.

--- entities/characters/hera/hera.gd
+++ entities/characters/hera/hera.gd
@@ -2,17 +2,30 @@
 extends Node2D
 
 var hera_active: bool = true
-var current_ability: int = 0
+
+func testHera():
+	DataManager.reset_game()
+	DataManager.unlock_ability(DataManager.Characters.HERA, DataManager.HeraAbility.STATE_WEAPON)
+	DataManager.unlock_ability(DataManager.Characters.HERA, DataManager.HeraAbility.STATE_SHIELD)
+	DataManager.unlock_ability(DataManager.Characters.HERA, DataManager.HeraAbility.STATE_PLATFORM)
+	DataManager.progress.selected_abilities[DataManager.Characters.HERA] = DataManager.HeraAbility.STATE_PLATFORM
 
 func _ready():
-	pass
+	# Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	testHera()
 
 func _input(event):
 	if event.is_action_pressed("hera-activate"):
-		hera_active = false
+		if DataManager.progress.selected_abilities[DataManager.Characters.HERA] == DataManager.HeraAbility.STATE_PLATFORM:
+			hera_platform()
 	if event.is_action_pressed("hera-toggle"):
 		DataManager.switch_ability(DataManager.Characters.HERA)
 
+func hera_platform():
+	hera_active = false
+	await get_tree().create_timer(0.5).timeout
+	hera_active = true
+
 func _physics_process(_delta: float) -> void:
 	if hera_active:
-		position += (get_global_mouse_position() - position) / 5 # Follows the mouse with delay.
+		position += (get_global_mouse_position() - position) / 5
