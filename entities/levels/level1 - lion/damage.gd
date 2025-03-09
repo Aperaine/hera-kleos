44f8938249c5d7831cb85fd6d75f7660a765e38e
Umarik <umarbektokyo@icloud.com>
2025-03-10 00:13:26 +0900

    Added Timers to the levels and now death counter is functional

--- entities/levels/level1 - lion/damage.gd
+++ entities/levels/level1 - lion/damage.gd
@@ -2,5 +2,6 @@ extends Node2D
 
 func _on_damage_collider_body_entered(body: CharacterBody2D) -> void:
 	if body.name == "Heracle":
+		DataManager.game_stats["deaths"] += 1
 		print("damage!")
 		DataManager.ram["heracle_dead"] = true
