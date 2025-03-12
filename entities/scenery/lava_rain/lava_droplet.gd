180440076f4254f90fe0dcf7c6136afed5895856
Umarik <umarbektokyo@icloud.com>
2025-03-12 21:43:23 +0900

    lava rain elements added.

---
+++ entities/scenery/lava_rain/lava_droplet.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+const FALL_SPEED = 500
+var active: bool = true
+
+func _physics_process(delta: float) -> void:
+	if active: position.y += FALL_SPEED * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Heracle":
+		DataManager.game_stats["deaths_heracle"] += 1
+		DataManager.ram["heracle_dead"] = true
+		queue_free()
+	if body.collision_layer & 1:
+		active = false
+		await get_tree().create_timer(0.2).timeout
+		queue_free()
