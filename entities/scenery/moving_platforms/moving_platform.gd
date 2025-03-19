dce87341d643ef205623a8def390f51ff21d9ef0
Umarik <umarbektokyo@icloud.com>
2025-03-19 22:28:26 +0900

    MovingPlatforms3.0

--- entities/scenery/moving_platforms/moving_platform.gd
+++ entities/scenery/moving_platforms/moving_platform.gd
@@ -5,15 +5,60 @@ enum MovementStyle {
 	linear,
 }
 
+enum MovementDirection {
+	horizontal,
+	vertical,
+}
+
+enum ContainerShape {
+	Brick,
+	Stick,
+	Brick_Stand,
+}
+
 @export var speed: float = 1.0
 @export var distance: float = 500
 @export var movement: MovementStyle
-#@export var is_jumpable: bool = false
+@export var movement_direction: MovementDirection = MovementDirection.horizontal
+@export var shape: ContainerShape = ContainerShape.Brick
+@export var damages_heracles: bool = true
+@export var damages_hera: bool = false
 
 var start_position: Vector2 
 
 func _ready() -> void:
 	start_position = position
+	if damages_hera:
+		if (shape == ContainerShape.Brick) or (shape == ContainerShape.Brick_Stand):
+			$"Obstacle Hera/Horizontal".disabled = false
+			$"Obstacle Hera/Vertical".disabled = true
+		else:
+			$"Obstacle Hera/Horizontal".disabled = false
+			$"Obstacle Hera/Vertical".disabled = true
+	else:
+		$"Obstacle Hera/Horizontal".disabled = true
+		$"Obstacle Hera/Vertical".disabled = true
+	if damages_heracles:
+		if shape == ContainerShape.Brick:
+			$"Obstacle Heracle/Horizontal".disabled = false
+			$"Obstacle Heracle/Vertical".disabled = true
+			$"Obstacle Heracle/HolderLeft".disabled = true
+			$"Obstacle Heracle/HolderRight".disabled = true
+		elif shape == ContainerShape.Stick:
+			$"Obstacle Heracle/Horizontal".disabled = true
+			$"Obstacle Heracle/Vertical".disabled = false
+			$"Obstacle Heracle/HolderLeft".disabled = true
+			$"Obstacle Heracle/HolderRight".disabled = true
+		elif shape == ContainerShape.Brick_Stand:
+			$"Obstacle Heracle/Horizontal".disabled = false
+			$"Obstacle Heracle/Vertical".disabled = true
+			$"Obstacle Heracle/HolderLeft".disabled = false
+			$"Obstacle Heracle/HolderRight".disabled = false
+	else:
+		$"Obstacle Heracle/Horizontal".disabled = true
+		$"Obstacle Heracle/Vertical".disabled = true
+		$"Obstacle Heracle/HolderLeft".disabled = true
+		$"Obstacle Heracle/HolderRight".disabled = true
 
 func _process(delta: float) -> void:
 	var t = Time.get_ticks_msec() / 1000.0 * speed
@@ -25,9 +70,7 @@ func _process(delta: float) -> void:
 	elif movement == MovementStyle.linear:
 		offset = pingpong(t * distance, distance) - (distance / 2)
 	
-	#if is_jumpable:
-		#pass
-	#else:
-		#pass
-	
-	position.x = start_position.x + offset
+	if movement_direction == MovementDirection.horizontal:
+		position.x = start_position.x + offset
+	else:
+		position.y = start_position.y + offset
