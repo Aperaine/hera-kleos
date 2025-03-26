0cca4f2aec6da362f17af5fe6e686ff2fbeb504a
Umarik <umarbektokyo@icloud.com>
2025-03-26 13:28:44 +0900

    Updates to the file structure

---
+++ entities/levels/level_scripts/teleporter.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var teleport_to: DataManager.ROOMS
+var area2d_node: Area2D
+
+func _ready():
+	add_to_group("gates")
+	area2d_node = $Area2D
+
+func _process(_delta):
+	for body in area2d_node.get_overlapping_bodies():
+		if body.name == "Heracle":
+			DataManager.change_scene(teleport_to)
