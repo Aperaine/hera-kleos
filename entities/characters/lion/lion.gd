542de51f9e1f4d66a98066cf6333b198af6e11d9
Umarik <umarbektokyo@icloud.com>
2025-03-27 21:06:32 +0900

    Updates to the boss battle

--- entities/characters/lion/lion.gd
+++ entities/characters/lion/lion.gd
@@ -5,6 +5,7 @@ enum attack_patterns {
 	SCREAM,
 	WALK,
 	ROCKS,
+	JUMP,
 	DASH,
 }
 
@@ -14,6 +15,7 @@ var lion_ram = { # these values are all random; adjust whatever you need Jafar
 	left_boundary = 0,
 	right_boundary = 1920,
 	init_pos = Vector2(500,500),
+	jump_locations = [Vector2(500,500), Vector2(200,200)] # maybe there'll be two locations into which he's jumping?
 }
 
 func _ready() -> void:
@@ -25,16 +27,22 @@ func _ready() -> void:
 	$AnimationPlayer.speed_scale = 0.6
 
 func _process(delta: float) -> void:
-	handle_health_phases()
+	handle_phases()
 
+func handle_phases():
+	var health = DataManager.ram["boss_health"]
+	if health in range(70,100):
+		phase1()
+	elif health in range(40,69):
+		phase2()
+	elif health in range(1,39):
+		phase3()
+	elif health <= 0:
+		phased()
+
+# battle phases
 func stage0():
-	$AnimationPlayer.play("Scream", -1.0, 0.2)
-	await $AnimationPlayer.animation_finished
-	$AnimationPlayer.play("Idle", -1.0, 1)
-	await get_tree().create_timer(1).timeout
-	$AnimationPlayer.stop()
-	DataManager.ram["boss_health"] = 100
-	segment = 1
+	scream()
 
 func phase1():
 	if segment == 1:
@@ -60,14 +68,34 @@ func phase3():
 
 func phased():
 	$FlipHelper/Sprite.modulate.a = 0.5
-	
-func handle_health_phases():
-	var health = DataManager.ram["boss_health"]
-	if health in range(70,100):
-		phase1()
-	elif health in range(40,69):
-		phase2()
-	elif health in range(1,39):
-		phase3()
-	elif health <= 0:
-		phased()
+
+# attack patterns
+func attack_idle():
+	pass
+
+func idle():
+	pass
+
+func scream():
+	$AnimationPlayer.play("Scream", -1.0, 0.2)
+	await $AnimationPlayer.animation_finished
+	$AnimationPlayer.play("Idle", -1.0, 1)
+	await get_tree().create_timer(1).timeout
+	$AnimationPlayer.stop()
+	DataManager.ram["boss_health"] = 100
+	segment = 1
+
+func walk():
+	pass
+
+func rocks():
+	var rock_prefab = preload("res://entities/characters/lion/falling_rock.tscn")
+	var one_rock = rock_prefab.instance()
+	one_rock.position = Vector2(400, 300)
+	get_parent().add_child(one_rock)
+
+func jump():
+	pass
+
+func dash():
+	pass
