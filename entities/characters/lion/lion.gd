86d1a3804a5a6239c7bacacc7b9dd6b6ab1a563e
Jafaryhuman <jafar.ali.gafurdjanov@gmail.com>
2025-03-27 16:01:36 +0500

    i hate lions

--- entities/characters/lion/lion.gd
+++ entities/characters/lion/lion.gd
@@ -16,9 +16,25 @@ func _process(delta: float) -> void:
 		$AnimationPlayer.play("Scream", -1.0, 0.2)
 		await $AnimationPlayer.animation_finished
 		$AnimationPlayer.play("Idle", -1.0, 1)
+		await get_tree().create_timer(1).timeout
+		$AnimationPlayer.stop()
 		DataManager.ram["boss_health"] = 100
+		segment = 1
 	elif 70 < DataManager.ram["boss_health"] and DataManager.ram["boss_health"] <= 100:
-		position.x -= 1
+		if segment == 1:
+			if not $AnimationPlayer.is_playing():
+				$AnimationPlayer.play("Walk")
+			position.x -= 10
+			if position.x <= -655:
+				$AnimationPlayer.play("Idle")
+				await get_tree().create_timer(0.5).timeout
+				$FlipHelper.scale.x = 1
+				await get_tree().create_timer(0.5).timeout
+				$AnimationPlayer.stop()
+				segment = 2
+		elif segment == 2:
+			$AnimationPlayer.play("Walk")
+			position.x += 10
 	elif 40 <DataManager.ram["boss_health"] and DataManager.ram["boss_health"] <= 70:
 		$AnimationPlayer.play("Idle")
 	elif 0 < DataManager.ram["boss_health"] and DataManager.ram["boss_health"] <= 40:
