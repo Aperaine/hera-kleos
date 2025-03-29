6f78b5cb3b01820295124e912c0f370d034918b7
Umarik <umarbektokyo@icloud.com>
2025-03-29 20:22:55 +0900

    lion updates

--- entities/characters/lion/lion.gd
+++ entities/characters/lion/lion.gd
@@ -9,13 +9,12 @@ enum attack_patterns {
 }
 
 var area2d_node: Area2D
-var segment: int = 1
-# these values are all random; adjust whatever you need Jafar
+var stage: int = 1
 const left_boundary = -655
 const right_boundary = 712
 const init_pos = Vector2(500,500)
-const jump_locations = [Vector2(500,500), Vector2(200,200)] # maybe there'll be two locations into which he's jumping?
-
+var moving = false
+const speed = 10
 
 func _ready() -> void:
 	area2d_node = $FlipHelper/Collider
@@ -23,35 +22,40 @@ func _ready() -> void:
 	print("Boss health set to:")
 	print(DataManager.ram["boss_health"])
 	$FlipHelper.scale.x = -1
+	#dumb_process()
+
+func dumb_process():
 	scream()
-	DataManager.ram["boss_health"] = 100
-	segment = 1
+	walk("right")
+	walk("left")
 
 func _process(delta: float) -> void:
 	handle_phases()
 
 func handle_phases():
-	var health = DataManager.ram["boss_health"]
-	if health in range(70,101):
-		phase1()
-	elif health in range(40,70):
-		phase2()
-	elif health in range(1,40):
-		phase3()
-	elif health <= 0:
-		phased()
+	if not moving:
+		var health = DataManager.ram["boss_health"]
+		if health in range(70,101):
+			phase1()
+		elif health in range(40,70):
+			phase2()
+		elif health in range(1,40):
+			phase3()
+		elif health <= 0:
+			phased()
 
 # battle phases
 func stage0():
 	scream()
 
 func phase1():
-	if segment == 1:
-		walk()
-	elif segment == 2:
-		$AnimationPlayer.play("Walk")
-		position.x += 1
-	
+	if stage == 1:
+		scream()
+		stage += 1
+	elif stage == 2:
+		walk("right")
+		stage += 1
+
 func phase2():
 	$AnimationPlayer.play("Idle")
 
@@ -69,31 +73,62 @@ func idle():
 	pass
 
 func scream():
+	moving = true
 	$AnimationPlayer.play("Scream", -1.0, 0.2)
 	await $AnimationPlayer.animation_finished
 	$AnimationPlayer.play("Idle", -1.0, 1)
 	await get_tree().create_timer(1).timeout
 	$AnimationPlayer.stop()
+	moving = false
 
-func walk():
-	$AnimationPlayer.play("Walk")
-	while position.x > left_boundary:
-		position.x -= 1
-		await get_tree().process_frame 
-	$AnimationPlayer.play("Idle")
-	await get_tree().create_timer(0.5).timeout
-	$FlipHelper.scale.x = 1
-	await get_tree().create_timer(0.5).timeout
-	$AnimationPlayer.stop()
+func walk(direction):
+	if direction == "right":
+		$FlipHelper.scale.x = -1
+		moving = true
+		$FlipHelper/Body/Head1.disabled = true
+		$FlipHelper/Body/Head2.disabled = true
+		$AnimationPlayer.play("Walk", -1, 0.7)
+		while position.x > left_boundary:
+			position.x -= speed
+			await get_tree().process_frame
+		$FlipHelper/Body/Head1.disabled = false
+		$FlipHelper/Body/Head2.disabled = false
+		$AnimationPlayer.play("Idle")
+		await get_tree().create_timer(0.5).timeout
+		$FlipHelper.scale.x = 1
+		await get_tree().create_timer(0.5).timeout
+		$AnimationPlayer.stop()
+		moving = false
+	elif direction == "left":
+		$FlipHelper.scale.x = 1
+		moving = true
+		$FlipHelper/Body/Head1.disabled = true
+		$FlipHelper/Body/Head2.disabled = true
+		$AnimationPlayer.play("Walk", -1, 0.7)
+		while position.x < right_boundary:
+			position.x += speed
+			await get_tree().process_frame
+		$FlipHelper/Body/Head1.disabled = false
+		$FlipHelper/Body/Head2.disabled = false
+		$AnimationPlayer.play("Idle")
+		await get_tree().create_timer(0.5).timeout
+		$FlipHelper.scale.x = -1
+		await get_tree().create_timer(0.5).timeout
+		$AnimationPlayer.stop()
+		moving = false
 
 func rocks():
+	moving = true
 	var rock_prefab = preload("res://entities/characters/lion/falling_rock.tscn")
 	var one_rock = rock_prefab.instance()
 	one_rock.position = Vector2(400, 300)
 	get_parent().add_child(one_rock)
+	moving = false
 
 func jump():
-	pass
+	moving = true
+	
+	moving = false
 
 func dash():
 	pass
