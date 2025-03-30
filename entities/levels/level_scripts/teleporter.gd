dd007279783ec50d1b2889d30082eb4323141012
Umarik <umarbektokyo@icloud.com>
2025-03-30 22:32:01 +0900

    Created teleporters

--- entities/levels/level_scripts/teleporter.gd
+++ entities/levels/level_scripts/teleporter.gd
@@ -1,7 +1,9 @@
 extends Node
 
 @export var teleport_to: DataManager.ROOMS
+@export var unlock: DataManager.ROOMS
 var area2d_node: Area2D
+var sound_file = "res://sounds/music/city_choir.mp3"
 
 func _ready():
 	add_to_group("gates")
@@ -11,4 +13,14 @@ func _ready():
 func _process(_delta):
 	for body in area2d_node.get_overlapping_bodies():
 		if body.name == "Heracle":
-			DataManager.change_scene(teleport_to)
+			DataManager.unlock_level(unlock)
+			teleport()
+
+func teleport():
+	await get_tree().process_frame
+
+	var tween = create_tween()
+	tween.tween_property($ColorRect, "modulate:a", 1.0, 1.5)
+	await tween.finished
+
+	await DataManager.change_scene(teleport_to)
