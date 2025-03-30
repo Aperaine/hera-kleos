61565a07b76a4d94e22fab8a74e18068fb9fac2d
Umarik <umarbektokyo@icloud.com>
2025-03-30 22:08:54 +0900

    Debugging sound issues

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hera-kleos
  
+Hera Kleos is a game developed by Jaunty games team for the juice gamejam. After working on it for some time, we have decided to build this into a bigger project with 
--- entities/levels/level_scripts/teleporter.gd
+++ entities/levels/level_scripts/teleporter.gd
@@ -6,6 +6,7 @@ var area2d_node: Area2D
 func _ready():
 	add_to_group("gates")
 	area2d_node = $Area2D
+	$ColorRect.modulate.a = 0.0
 
 func _process(_delta):
 	for body in area2d_node.get_overlapping_bodies():
